5b01fcfe6f0794016f7fa6388b054d843423b896
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-12-05 05:26:49 +0600

    [front] Added a toolbar and integrated axiosAPI.ts for API requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,17 @@
-const App = () => <></>;
+import Navbar from "./components/Navbar/Navbar.tsx";
+import Form from "./components/Form/Form.tsx";
+
+const App = () =>{
+    return (
+        <>
+            <header>
+                <Navbar/>
+            </header>
+            <main>
+                <Form/>
+            </main>
+        </>
+    );
+};
 
 export default App;
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import {AppBar, Box, Container, Toolbar, Typography,} from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, mb: 5 }}>
+        <AppBar
+          position="static"
+          sx={{
+            padding: "10px",
+            borderBottom: "1px solid",
+              backgroundColor: "#122549",
+          }}
+        >
+          <Toolbar>
+            <Container>
+              <Typography
+                variant="h5"
+                sx={{ flexGrow: 1, textDecoration: "none", color: "#ffff" }}
+              >
+                  CryptSMS
+              </Typography>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
